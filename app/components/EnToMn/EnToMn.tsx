f16a24bc307e320d82894bce40f9bf3c87dd8e26
baska <bbaska837@gmail.com>
2023-05-03 18:02:21 +0800

    add

--- app/components/EnToMn/EnToMn.tsx
+++ app/components/EnToMn/EnToMn.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import SectionOne from "./SectionOne";
 import SectionTwo from "./SectionTwo";
 
+
 const EnToMn = () => {
   return (
-    <div className="grid grid-cols-[30%_15%_55%] w-full h-[30rem] gap-3 mt-[9rem] bg-white">
+    <div className="grid grid-cols-[30%_15%_55%] w-full h-[30rem] gap-3 mt-[9rem] bg-white bg-[url('./img/bg2.jpg')] bg-cover bg-no-repeat bg-center">
       <SectionOne />
       <SectionTwo />
       <div></div>
